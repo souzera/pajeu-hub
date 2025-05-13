fada99687edb4461ac5d2d9c1ff1ace2622659a5
matheus.unix <ps.matheusb@gmail.com>
2025-05-13 13:21:19 -0300

    :truck: create sport initial routes

--- backend/src/main/java/com/pajeuhub/backend/core/entities/Activity.java
+++ backend/src/main/java/com/pajeuhub/backend/core/entities/Activity.java
@@ -2,8 +2,6 @@ package com.pajeuhub.backend.core.entities;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
-
 
 public record Activity(
     Long id,
@@ -15,14 +13,4 @@ public record Activity(
     List<Player> members,
     boolean status
 ) {
-    // public Activity(Long id, String info, Date date, Place place, Player creator, Sport sport, List<Player> members, boolean status) {
-    //     this.id = id;
-    //     this.info = info;
-    //     this.date = date;
-    //     this.place = place;
-    //     this.creator = creator;
-    //     this.sport = sport;
-    //     this.members = members;
-    //     this.status = status;
-    // }
 }
