1b7980ea1b7f59fe3a642aac88d300f087e47812
matheus <ps.matheusb@gmail.com>
2025-05-17 14:01:21 -0300

    :bug: fixes refer usecases

--- backend/src/main/java/com/pajeuhub/backend/infra/presentation/PlayerController.java
+++ backend/src/main/java/com/pajeuhub/backend/infra/presentation/PlayerController.java
@@ -1,33 +1,61 @@
 package com.pajeuhub.backend.infra.presentation;
 
-import com.pajeuhub.backend.infra.mappers.PlayerMapper;
-import com.pajeuhub.backend.core.usecases.player.CreatePlayerUseCase;
-import com.pajeuhub.backend.core.usecases.player.FindPlayerUseCase;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pajeuhub.backend.core.entities.Player;
+import com.pajeuhub.backend.core.usecases.player.CreatePlayerCase;
+import com.pajeuhub.backend.core.usecases.player.FindPlayerCase;
+import com.pajeuhub.backend.infra.dto.PlayerDTO;
+import com.pajeuhub.backend.infra.mapper.PlayerMapper;
+
+@RestController
 public class PlayerController {
     
     private final PlayerMapper playerMapper;
 
-    private final CreatePlayerUseCase createPlayerUseCase;
-    private final FindPlayerUseCase findPlayerUseCase;
+    private final CreatePlayerCase createPlayerCase;
+    private final FindPlayerCase findPlayerCase;
 
     public PlayerController(
-        CreatePlayerUseCase createPlayerUseCase,
-        FindPlayerUseCase findPlayerUseCase
+        CreatePlayerCase createPlayeCase,
+        FindPlayerCase findPlayerCase
     ){
         this.playerMapper = new PlayerMapper();
 
-        this.createPlayerUseCase = createPlayerUseCase;
-        this.findPlayerUseCase = findPlayerUseCase;
+        this.createPlayerCase = createPlayeCase;
+        this.findPlayerCase = findPlayerCase;
     }
 
+    @PostMapping
     public ResponseEntity<Map<String, Object>> createPlayer(
         @RequestBody PlayerDTO playerDTO
     ){
-        Player player = createPlayerUseCase.execute(playerMapper.toDomain(playerDTO));
+        Player player = createPlayerCase.execute(playerMapper.toDomain(playerDTO));
 
         Map<String, Object> response = new HashMap<>();
         response.put("message", "Player created successfully");
+        response.put("player", playerMapper.toDTO(player));
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> findPlayerById(
+        @PathVariable("id")
+        String id
+    ){
+        Player player = findPlayerCase.execute(id);
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Player found successfully");
         response.put("player", playerMapper.toDTO(player));
 
         return ResponseEntity.ok(response);
